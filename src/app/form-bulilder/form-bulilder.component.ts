ce20e83ddc6e1151da74c827c62ab9b1a9ebe088
Mohsen Shafiey <mohsen.shafiey@stratosphere-games.com>
2024-02-29 12:59:02 +0100

    add:Add MatSlideToggleModule to form-bulilder.component.ts

--- src/app/form-bulilder/form-bulilder.component.ts
+++ src/app/form-bulilder/form-bulilder.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-form-bulilder',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule],
   templateUrl: './form-bulilder.component.html',
-  styleUrl: './form-bulilder.component.scss'
+  styleUrl: './form-bulilder.component.scss',
 })
-export class FormBulilderComponent {
-
-}
+export class FormBulilderComponent {}
