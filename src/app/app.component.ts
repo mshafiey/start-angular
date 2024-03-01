7b1501dfc36d907e40aa9aa024812084fafe4084
Mohsen Shafiey <mohsen.shafiey@stratosphere-games.com>
2024-03-01 11:12:33 +0100

    refactor:Delete unused components and update app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormBulilderComponent } from './form-bulilder/form-bulilder.component';
-import { Form1Component } from './form1/form1.component';
-import { DahsboardComponent } from './dahsboard/dahsboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    FormBulilderComponent,
-    Form1Component,
-    DahsboardComponent,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
