c13af901fb03f1c30cfaeeaa459117830b2795da
Mohsen Shafiey <mohsen.shafiey@stratosphere-games.com>
2024-03-07 14:24:06 +0100

    feat:Add toolbar and sidenav to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+
+import {
+  MatSidenavModule,
+  MatDrawer,
+  MatSidenavContainer,
+  MatSidenavContent,
+} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDrawer,
+    MatSidenavContainer,
+    MatSidenavContent,
+    MatListModule,
+    MatSidenavModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
