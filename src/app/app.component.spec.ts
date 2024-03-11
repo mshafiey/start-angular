bb42cf7fbb8dafe664c72634f75d41ec135323b4
Mohsen Shafiey <mohsen.shafiey@stratosphere-games.com>
2024-03-11 12:19:01 +0100

    feat: Update project configuration and styles

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'start-angular' title`, () => {
+  it(`should have the 'start' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('start-angular');
+    expect(app.title).toEqual('start');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, start-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, start');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
-
-import {
-  MatSidenavModule,
-  MatDrawer,
-  MatSidenavContainer,
-  MatSidenavContent,
-} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    MatIconModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatDrawer,
-    MatSidenavContainer,
-    MatSidenavContent,
-    MatListModule,
-    MatSidenavModule,
-    MatButtonModule,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'start-angular';
+  title = 'start';
 }
