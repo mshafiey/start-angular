bc552afc57eca3699a71a71d78098464934d9cf1
Mohsen Shafiey <mohsen.shafiey@stratosphere-games.com>
2024-03-13 11:25:37 +0100

    feat: Add Tinycard component to app.component.html and import it in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { AsideComponent } from './component/aside/aside.component';
 import { NavComponent } from './component/nav/nav.component';
 import { WidecardComponent } from './component/widecard/widecard.component';
+import { TinycardComponent } from './component/tinycard/tinycard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsideComponent, NavComponent, WidecardComponent],
+  imports: [
+    RouterOutlet,
+    AsideComponent,
+    NavComponent,
+    WidecardComponent,
+    TinycardComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/component/tinycard/tinycard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tinycard',
+  standalone: true,
+  imports: [],
+  templateUrl: './tinycard.component.html',
+  styleUrl: './tinycard.component.scss'
+})
+export class TinycardComponent {
+
+}
