c4734c72ffb560b1918432b9848daab22422f1da
Mohsen Shafiey <mohsen.shafiey@stratosphere-games.com>
2024-03-13 15:23:38 +0100

    feat:Add new route and update links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PageTestComponent } from './pages/page-test/page-test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'page-test', component: PageTestComponent },
+];
