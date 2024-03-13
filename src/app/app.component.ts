ce31ed5f8b5f824dfdd8f08c215913db33c6d1d3
Mohsen Shafiey <mohsen.shafiey@stratosphere-games.com>
2024-03-13 10:39:26 +0100

    config:Add skipTests option to component schematics and update page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AsideComponent } from './aside/aside.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsideComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'start';
