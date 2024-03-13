c44d851e0a46a258ebd7bad471dd9128c4ee20d9
Mohsen Shafiey <mohsen.shafiey@stratosphere-games.com>
2024-03-13 11:51:23 +0100

    Refactor: component imports and file structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AsideComponent } from './component/aside/aside.component';
-import { NavComponent } from './component/nav/nav.component';
+import { AsideComponent } from './layout/aside/aside.component';
+import { NavComponent } from './layout/nav/nav.component';
 import { WidecardComponent } from './component/widecard/widecard.component';
 import { TinycardComponent } from './component/tinycard/tinycard.component';
 import { BigcardComponent } from './component/bigcard/bigcard.component';
