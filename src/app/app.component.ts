9f88e207418d37fff7cf2b90b63ed9a298cdba96
Mohsen Shafiey <mohsen.shafiey@stratosphere-games.com>
2024-03-13 10:45:20 +0100

    Refactor navbar component and add NavComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AsideComponent } from './aside/aside.component';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsideComponent],
+  imports: [RouterOutlet, AsideComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
